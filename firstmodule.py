d52ac9a8696fcc09ce34705ca211e643a86354ad
matthiashuber <huber@ifo.de>
2017-04-20 18:39:37 +0200

    Added files for .bat and for modules Changes in Functions
    Signed-off-by: matthiashuber <huber@ifo.de>

---
+++ firstmodule.py
@@ -0,0 +1,9 @@
+ #This is a new module called firstownmodule
+def sum_squared(x, y):
+    return x**2 + y**2
+
+def reverse_order(ls):
+    return ls[::-1]
+
+def all_men_must_die():
+    print("Valar Morghulis!")
